7027bb91b3907ee32ef00a768232082d89a79127
skyoo1226 <you1226@naver.com>
2022-02-25 05:19:36 +0900

    if 조건문 입력

--- app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
+++ app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +14,31 @@ class MainActivity : AppCompatActivity() {
         // 참조, 메모 시작... 아이디, 비번, 로그인...
         // 자동 완성 잘 안됨...추후 문의
 
-        btnLog.setOnClickListner{
-            Log.d("메인화면","로그버튼 눌림")
+        btnLogin.setOnClickListener {
 
-        }
-        
-        btnToast.setOnClickListner{
+            val id = "admin"
+            val password = "qwer"
 
-            Toast.makeText(this, "토스트 누르기", Toast.LENGTH_SHORT).show()
+            if (id == edtId && password == edtPassword) {
+
+                Toast.makeText(this,"관리자님 환영합니다.", Toast.LENGTH_SHORT). show()
+
+            }
+
+            else if (password == edtPassword) {
+
+                Toast.makeText(this,"아이디가 틀립니다.", Toast.LENGTH_SHORT). show()
+            }
+
+            else if (id == edtId) {
+
+                Toast.makeText(this,"비번이 잘못되었습니다.", Toast.LENGTH_SHORT). show()
+            }
+
+            else {
+
+                Toast.makeText(this,"로그인에 실패하였습니다.", Toast.LENGTH_SHORT). show()
+            }
 
         }
 
