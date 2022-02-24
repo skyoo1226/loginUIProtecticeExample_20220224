5fff92c881e184812b83617d03b8ef5d7753c0a1
skyoo1226 <you1226@naver.com>
2022-02-25 05:38:45 +0900

    if 조건문 입력

--- app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
+++ app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
@@ -19,18 +19,18 @@ class MainActivity : AppCompatActivity() {
             val myId = "admin"
             val myPassword = "qwer"
 
-            if (myId == edtId && myPassword == edtPassword) {
+            if (myId == "edtId" && myPassword == "edtPassword") {
 
                 Toast.makeText(this,"관리자님 환영합니다.", Toast.LENGTH_SHORT). show()
 
             }
 
-            else if (myId != edtId) {
+            else if (myId != "edtId") {
 
                 Toast.makeText(this,"아이디가 틀립니다.", Toast.LENGTH_SHORT). show()
             }
 
-            else if (myPassword != edtPassword) {
+            else if (myPassword != "edtPassword") {
 
                 Toast.makeText(this,"비번이 잘못되었습니다.", Toast.LENGTH_SHORT). show()
             }
