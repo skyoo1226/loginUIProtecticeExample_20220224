8542648dd332e870dc61667498ce2cc1add4544a
skyoo1226 <you1226@naver.com>
2022-02-24 17:13:04 +0900

    메인 자바 작업 시작

--- app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
+++ app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
@@ -2,10 +2,29 @@ package com.skyoo.loginuiprotecticeexample_20220224
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 참조, 메모 시작... 아이디, 비번, 로그인...
+        // 자동 완성 잘 안됨...추후 문의
+
+        btnLog.setOnClickListner{
+            Log.d("메인화면","로그버튼 눌림")
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
