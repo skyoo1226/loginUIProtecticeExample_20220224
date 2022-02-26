3b779ccba106b6a2103e675bcf5ac32d2e5311a4
skyoo1226 <you1226@naver.com>
2022-02-26 20:20:40 +0900

    로그인 버튼 생성 시 따로 linearLayout 지정 안해도 될것 같음 - 수정 함.

--- app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
+++ app/src/main/java/com/skyoo/loginuiprotecticeexample_20220224/MainActivity.kt
@@ -13,33 +13,52 @@ class MainActivity : AppCompatActivity() {
 
         // 참조, 메모 시작... 아이디, 비번, 로그인...
         // 자동 완성 잘 안됨...추후 문의
+        // 2월 26일 복습으로 에러 수정
 
         btnLogin.setOnClickListener {
 
-            val myId = "admin"
-            val myPassword = "qwer"
+            val inputID = edtId.text.toString()
+            val inputPw = edtId.text.toString()
 
-            if (myId == edtId && myPassword == edtPassword) {
+            if (inputID == "admin" && inputPw == "qwer") {
                 Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
-
             }
-
-            else if (myId != edtId) {
-                Toast.makeText(this, "아이디가 틀립니다.", Toast.LENGTH_SHORT).show()
-
-            }
-
-            else if (myPassword != edtPassword) {
-                Toast.makeText(this, "비번이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
-                
-            }
-
             else {
-                Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
             }
+
 
         }
 
     }
 }
+
+//        btnLogin.setOnClickListener {
+//
+//            val myId = "admin"
+//            val myPassword = "qwer"
+//
+//            if (myId == edtId && myPassword == edtPassword) {
+//                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//            else if (myId != edtId) {
+//                Toast.makeText(this, "아이디가 틀립니다.", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//            else if (myPassword != edtPassword) {
+//                Toast.makeText(this, "비번이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//            else {
+//                Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+//
+//            }
+//
+//        }
+//
+//    }
+
